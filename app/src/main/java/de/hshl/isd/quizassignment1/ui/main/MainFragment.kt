8e1d92f1dda9a274a119ea9cf9c08f65eb5c1b23
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-07 09:29:48 +0100

    Added example questions

--- app/src/main/java/de/hshl/isd/quizassignment1/ui/main/MainFragment.kt
+++ app/src/main/java/de/hshl/isd/quizassignment1/ui/main/MainFragment.kt
@@ -2,6 +2,7 @@ package de.hshl.isd.quizassignment1.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,6 +25,8 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        Log.i("MainFragment",viewModel.questions.toString())
         // TODO: Use the ViewModel
     }
 
--- app/src/main/java/de/hshl/isd/quizassignment1/ui/main/MainViewModel.kt
+++ app/src/main/java/de/hshl/isd/quizassignment1/ui/main/MainViewModel.kt
@@ -3,5 +3,12 @@ package de.hshl.isd.quizassignment1.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    val questions = listOf(Pair("Das Videospiel Donkey Kong sollte ursprünglich Popeye als Hauptfigur haben.", true),
+        Pair("Die Farbe Orange wurde nach der Frucht benannt.", true),
+        Pair("In der griechischen Mythologie ist Hera die Göttin der Ernte.", false),
+        Pair("Liechtenstein hat keinen eigenen Flughafen.", true),
+        Pair("Die meisten Subarus werden in China hergestellt.", false))
+
     // TODO: Implement the ViewModel
 }
