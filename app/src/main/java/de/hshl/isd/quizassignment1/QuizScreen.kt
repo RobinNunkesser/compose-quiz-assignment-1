528c1c50c998df92d32f2ea1bce0d707e6b5a660
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-08-19 10:41:21 +0200

    Jetpack Compose 1.0.0

--- app/src/main/java/de/hshl/isd/quizassignment1/QuizScreen.kt
+++ app/src/main/java/de/hshl/isd/quizassignment1/QuizScreen.kt
@@ -1,21 +1,10 @@
 package de.hshl.isd.quizcompose
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
 
 @Composable
 fun QuizScreen(navController: NavController, viewModel: MainViewModel) {
